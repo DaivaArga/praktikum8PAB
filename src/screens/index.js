7fe27cae5a59f0affd523fda7b67bdb7dfe812d5
DaivaArga <daivaarga14@gmail.com>
2025-12-12 05:05:11 +0700

    hy

---
+++ src/screens/index.js
@@ -0,0 +1,9 @@
+import Home from "./Home";
+import Profile from "./Profile_temp";
+import Add from "./Add";
+import Login from "./Login";
+import Register from "./Register_temp";
+import Splash from "./Splash";
+import EditNote from "./EditNote";
+
+export { Home, Profile, Add, Login, Register, Splash, EditNote };
